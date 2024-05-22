967def2675dd42fd9683eb1b3d6ec1925a471b0f
Matias Oliva <matiasolivab@uc.cl>
2024-05-22 18:34:59 -0400

    cors added

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,26 @@
 require('dotenv').config({ path: '../../.env' });
 const express = require('express');
+const cors = require('cors');
 const { produceRecommendation, getJobStatus } = require('./services/recommendationService');
+
+const corsOptions = {
+  origin: '*',
+  allowHeaders: [
+    'Access-Control-Allow-Headers',
+    'Origin',
+    'Accept',
+    'X-Requested-With',
+    'Content-Type',
+    'Access-Control-Request-Method',
+    'Access-Control-Request-Headers',
+    'Auth',
+  ],
+  allowMethods: ['GET', 'HEAD', 'OPTIONS', 'PUT', 'POST', 'DELETE', 'PATCH'],
+};
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 
 app.post('/job', produceRecommendation);
